4fb5bc41a13f0f095d3e965d627f013a1bbf51f6
Harsh <harsh@technoarchsoftwares.com>
2019-08-28 17:47:52 +0530

    Update Titles.js

--- src/components/Titles.js
+++ src/components/Titles.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Titles = () => (
             <div style= {{ margin: '94px'}} >
-                <h1>Weather Finder</h1>
+                <h1>Weather Finder v2</h1>
                 <p>Find out temperature Conditions and more...</p>
             </div>
         );
